b3e1529cd34101c4071d8dc1e4aae8d964246995
DIRANJF <38994971+DIRANJF@users.noreply.github.com>
2026-06-12 17:26:33 +0800

    修改启动项

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
-import uvicorn
+import asyncio
 from main import app
 
-if __name__ == "__main__":
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=8502,
-        limit_concurrency=100,
-        access_log=False,
-    )
+try:
+    import uvicorn
+
+    if __name__ == "__main__":
+        config = uvicorn.Config(
+            app,
+            host="0.0.0.0",
+            port=8502,
+            limit_concurrency=100,
+            access_log=False,
+        )
+        server = uvicorn.Server(config)
+        server.run()
+except ImportError:
+    print("uvicorn not installed, please install requirements.txt first")
